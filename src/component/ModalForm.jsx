83d46efca9ce434757d1fb7c0a9c507a728e500e
Shajalal27 <hpshajalal@gmail.com>
2024-05-13 16:49:10 +0600

    datePicker done

--- src/component/ModalForm.jsx
+++ src/component/ModalForm.jsx
@@ -18,7 +18,12 @@ const ModalForm = () => {
             image,
             name
          }
-         } = service;
+         } = service || {};
+
+    const handleBooking = async e =>{
+        e.preventDefault()
+
+    }
     return (
         <div className="hero h-[450px] bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse gap-12">
@@ -35,16 +40,20 @@ const ModalForm = () => {
                 <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
                     <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Account settings</h2>
 
-                    <form>
+                    <form onSubmit={handleBooking}>
                         <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                             <div>
                                 <label className="text-gray-700 dark:text-gray-200" >Username</label>
-                                <input id="username" name="name" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                                <input id="username" name="name" type="text" 
+                                 value={user?.displayName}
+                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
                                 <label className="text-gray-700 dark:text-gray-200" >Email Address</label>
-                                <input id="emailAddress" name="email" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                                <input id="emailAddress" name="email" type="email" 
+                                value={user?.email}
+                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
@@ -57,13 +66,13 @@ const ModalForm = () => {
                             </div>
 
                             <div>
-                                <label className="text-gray-700 dark:text-gray-200" >Password Confirmation</label>
-                                <input id="passwordConfirmation" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                                <label className="text-gray-700 dark:text-gray-200" >Price</label>
+                                <input id="price" name="price" type="number" value={service_price} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
                         </div>
 
                         <div className="flex justify-end mt-6">
-                            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+                            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Book Now</button>
                         </div>
                     </form>
                 </section>
