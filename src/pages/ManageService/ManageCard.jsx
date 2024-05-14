773e70cccf03df5ac4927e79ee7f1a038cfc707a
Shajalal27 <hpshajalal@gmail.com>
2024-05-14 12:04:15 +0600

    deleted service succcessfull done

--- src/pages/ManageService/ManageCard.jsx
+++ src/pages/ManageService/ManageCard.jsx
@@ -1,6 +1,7 @@
 
 
-const ManageCard = ({service}) => {
+
+const ManageCard = ({service, handleDelete}) => {
     const{_id, 
         service_image, 
         service_name,
@@ -8,18 +9,24 @@ const ManageCard = ({service}) => {
         service_price,
         service_provider
         } = service;
+
+    
     return (
        
         <div className="hero  bg-base-200 shadow-xl border-2 mt-12">
              <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="w-1/3">
                     <img className="w-[400px] h-[200px]" src={service_image} />
-                <div>
+                </div>
+                <div className="w-2/3">
                     <h1 className="text-5xl font-bold">{service_name}</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                   <div className="flex gap-8">
-                        <button className="btn btn-primary">Update</button>
-                        <button className="btn btn-primary">Delete</button>
-                   </div>
+                    <p className="py-6">{service_description}</p>
+                    <div className="flex gap-8">
+                         <button className="btn btn-primary">Update</button>
+                         <button onClick={() => handleDelete(_id)}
+                          className="btn btn-primary">Delete</button>
+                         <p className="ml-24 border bg-orange-400 p-2 font-bold flex justify-center items-center text-blue-700 rounded-md">Price:$ <span> {service_price}</span></p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/ManageService/ManageService.jsx
+++ src/pages/ManageService/ManageService.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import ManageCard from "./ManageCard";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 
 
@@ -7,11 +9,32 @@ import ManageCard from "./ManageCard";
 
 const ManageService = () => {
     const service = useLoaderData()
+    const[services, setServices] = useState()
+
+    const handleDelete = id =>{
+        const proceed = confirm('Are Your sure Delete');
+        if(proceed){
+            fetch(`${import.meta.env.VITE_API_URL}/service/${id} `, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data =>{
+                services(data)
+                if(data.deletedCount > 0){
+                    toast.success('Delete Successfull')
+                    const remaining = service.filter(services => services._id !== id)
+                    setServices(remaining)
+                }
+            })
+        }
+    }
     
     return (
        <div className="">
                 {
-                    service.map(service => <ManageCard key={service._id} service={service} />)
+                    service.map(service => <ManageCard key={service._id} service={service} 
+                    handleDelete={handleDelete}
+                    />)
                 }     
         </div>
        
